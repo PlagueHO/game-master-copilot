3774c06ac8005e9a02780fe0f7ee075dfab1bf12
Daniel Scott-Raynsford <PlagueHO@users.noreply.github.com>
2023-06-16 21:49:08 +0000

    Updated devcongtainer

--- .devcontainer/on-setup.sh
+++ .devcontainer/on-setup.sh
@@ -1,1 +1,3 @@
-dotnet dev-certs https --trust
+dotnet dev-certs https
+sudo -E dotnet dev-certs https -ep /usr/local/share/ca-certificates/aspnet/https.crt --format PEM
+sudo update-ca-certificates
